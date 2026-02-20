722f7b258b037e626f5ddde4e16b2b7ae66a7030
hienlong <hienlong106@gmail.com>
2026-02-20 06:21:28 +0000

    Remove commented import and reference for HybridMultiFrameOCR from models module

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,7 +1,7 @@
 """Models module containing network architectures."""
 from src.models.crnn import MultiFrameCRNN
 from src.models.restran import ResTranOCR
-from src.models.hybrid import HybridMultiFrameOCR
+# from src.models.hybrid import HybridMultiFrameOCR
 from src.models.components import (
     AttentionFusion,
     CNNBackbone,
@@ -12,7 +12,7 @@ from src.models.components import (
 __all__ = [
     "MultiFrameCRNN",
     "ResTranOCR",
-    "HybridMultiFrameOCR",
+    # "HybridMultiFrameOCR",
     "AttentionFusion",
     "CNNBackbone",
     "ResNetFeatureExtractor",
